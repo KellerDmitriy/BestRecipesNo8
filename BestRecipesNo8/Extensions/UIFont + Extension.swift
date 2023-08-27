785645d7f2c9432ea10f90c915162d4f76c046af
DmKeller <dmit.keller@gmail.com>
2023-08-28 00:25:04 +0300

    add Colors, Font, Button

--- BestRecipesNo8/Extensions/UIFont + Extension.swift
+++ BestRecipesNo8/Extensions/UIFont + Extension.swift
@@ -10,31 +10,31 @@ import UIKit
 extension UIFont {
     
     class var bolt56: UIFont {
-        return UIFont(name: "Poppins", size: 56) ??  UIFont.systemFont(ofSize: 56, weight: .bold)
+        return UIFont(name: "Poppins-Bold", size: 56) ??  UIFont.systemFont(ofSize: 56, weight: .bold)
     }
     
     class var bold48: UIFont {
-        return UIFont(name: "Poppins", size: 48) ??  UIFont.systemFont(ofSize: 48, weight: .bold)
+        return UIFont(name: "Poppins-Bold", size: 48) ??  UIFont.systemFont(ofSize: 48, weight: .bold)
     }
     
     class var bold40: UIFont {
-        return UIFont(name: "Poppins", size: 40) ??  UIFont.systemFont(ofSize: 40, weight: .bold)
+        return UIFont(name: "Poppins-Bold", size: 40) ??  UIFont.systemFont(ofSize: 40, weight: .bold)
     }
     
     class var bold32: UIFont {
-        return UIFont(name: "Poppins", size: 32) ??  UIFont.systemFont(ofSize: 32, weight: .bold)
+        return UIFont(name: "Poppins-Bold", size: 32) ??  UIFont.systemFont(ofSize: 32, weight: .bold)
     }
     
     class var bold24: UIFont {
-        return UIFont(name: "Poppins", size: 24) ??  UIFont.systemFont(ofSize: 24, weight: .bold)
+        return UIFont(name: "Poppins-Bold", size: 24) ??  UIFont.systemFont(ofSize: 24, weight: .bold)
     }
     
     class var bold16: UIFont {
-        return UIFont(name: "Poppins", size: 16) ??  UIFont.systemFont(ofSize: 16, weight: .bold)
+        return UIFont(name: "Poppins-Bold", size: 16) ??  UIFont.systemFont(ofSize: 16, weight: .bold)
     }
     
     class var regular16: UIFont {
-        return UIFont(name: "Poppins", size: 16) ??  UIFont.systemFont(ofSize: 16, weight: .regular)
+        return UIFont(name: "Poppins-Bold", size: 16) ??  UIFont.systemFont(ofSize: 16, weight: .regular)
     }
 
 }
