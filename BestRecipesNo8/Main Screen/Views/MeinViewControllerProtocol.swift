f6a9f2bb955f9ba3100589947fd26b70e72e70ec
Sbat2000 <zevzok@gmail.com>
2023-08-31 20:20:57 +0800

    add: MainPresenter & protocol, dataServiceProtocol

---
+++ BestRecipesNo8/Main Screen/Views/MeinViewControllerProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  MeinViewControllerProtocol.swift
+//  BestRecipesNo8
+//
+//  Created by Aleksandr Garipov on 31.08.2023.
+//
+
+import Foundation
+
+protocol MainScreenViewControllerProtocol: class {}
